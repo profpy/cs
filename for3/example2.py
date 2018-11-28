d85f716720d79ad4df5568c0a4091be470d242ed
pythonhs <39799978+pythonhs@users.noreply.github.com>
2018-11-28 05:30:04 -0500

    Create example2.py

---
+++ for3/example2.py
@@ -0,0 +1,6 @@
+print("Year\tIncrease in mm")                             
+print("-----------")                                      
+sum = 0                                                  
+for x in range(2018, 2018+26, 5):                       
+   print(x,"\t",sum)                                  
+   sum = sum + 1.7*5  
