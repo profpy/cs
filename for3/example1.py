3dc6f127693d7bb9b4fe4cb5e52c2b75a8e311e1
pythonhs <39799978+pythonhs@users.noreply.github.com>
2018-11-28 05:10:52 -0500

    Create example1.py

---
+++ for3/example1.py
@@ -0,0 +1,4 @@
+print("x\ty")                                             
+ print("-----------")                                     
+ for x in range(-5, 5+1):                               
+     print(x,"\t",2*x+1)                                   
