a00171e4397cc5e9ceae038a0ed4593c81aefad8
pythonhs <39799978+pythonhs@users.noreply.github.com>
2018-12-10 06:00:45 -0500

    Create examples2.py

---
+++ while/examples2.py
@@ -0,0 +1,21 @@
+print("Example 1")
+x = 3
+food = input("Favorite Food: ")
+while x > 0:
+    print(food)
+    x = x - 1
+
+print("Example 2")
+x = 10
+while x > 0:
+    print(x)
+    x = x - 2
+
+print("Example 3")
+print("Mins\tCalories")
+calories = 400
+x = 10
+while x >= 0:
+    print(x,"\t",calories)
+    calories = calories - 11
+    x = x - 1
