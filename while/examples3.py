80896559645c4f607f8b3681f9c639fc86a450a2
pythonhs <39799978+pythonhs@users.noreply.github.com>
2018-12-11 06:02:48 -0500

    Create examples3.py

---
+++ while/examples3.py
@@ -0,0 +1,47 @@
+print("Example 1")
+ride = 0
+sum = 0
+while ride >= 0:
+    ride = int(input("How long? "))
+    if ride > 0:
+        sum = sum + ride
+print("Sum:",sum)
+
+print("Example 2")
+ride = 0
+sum = 0
+x = 0
+while ride >= 0:
+    ride = int(input("How long? "))
+    if ride > 0:
+        sum = sum + ride
+        x = x + 1
+print("Average:",sum/x)
+
+print("Example 3")
+ride = 0
+max = 0
+x = 0
+while ride >= 0:
+    ride = int(input("How long? "))
+    if ride > 0 and x == 0:
+        max = ride
+    else:
+        if ride > max:
+            max = ride
+    x = x + 1
+print("Max:",max)
+
+print("Example 4")
+ride = 0
+min = 0
+x = 0
+while ride >= 0:
+    ride = int(input("How long? "))
+    if ride > 0 and x == 0:
+        min = ride
+    elif ride > 0:
+        if ride < min:
+            min = ride
+    x = x + 1
+print("Min:",min)
