b8c7937729d3b6e7bd98b5acd94a5ded7fdaeead
pythonhs <39799978+pythonhs@users.noreply.github.com>
2018-12-14 07:34:36 -0500

    Create day2.py

---
+++ for/day2.py
@@ -0,0 +1,34 @@
+done = False
+while not done:
+    print("Menu")
+    print("E1 - Example 1")
+    print("E2 - Example 2")
+    print("E3 - Example 3")
+    print("E4 - Example 4")
+    print("Q - Quit")
+    choice = input("Choose: ")
+    if choice == "E1":
+        for x in range(1,7):
+            print(i)
+    elif choice == "E2":
+        print("Number\tSquared")
+        for x in range(-5,6):
+            print(x,"\t",x**2)
+    elif choice == "E3":
+        count = 0
+        min = int(input("Min: "))
+        max = int(input("Max: "))
+        divisible = int(input("Divisible: "))
+        for x in range(min,max):
+            if x % divisible == 0:
+                count = count + 1
+        print("Count:",count)
+    elif choice == "E4":
+        for i in range(5):
+            num = int(input("Number: "))
+            if i == 2:
+                third = i
+        print(third)
+    elif choice == "Q":
+        print("Quitting!!")
+        done = True
